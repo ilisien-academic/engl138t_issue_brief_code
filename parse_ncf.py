e9b306031cbedca516ffbb61caf4d33cba1b56cd
ilisien-academic <irl5102@psu.edu>
2026-04-06 15:47:42 +0000

    Apr 6, 2026, 11:47 AM

--- parse_ncf.py
+++ parse_ncf.py
@@ -2,6 +2,9 @@ import xarray
 import rioxarray
 from rasterio.transform import from_origin
 
-def parse_ncf_to_tif(in_path,out_path,variable_of):
+def parse_emission_ncf_to_tif(in_path,out_path,voi):
     dataset = xarray.open_dataset(in_path)
 
+    only_voi = dataset["PM25ANN"].isel(TSTEP=0,LAY=0)
+
+    # parameters specific to the IOAPI format
