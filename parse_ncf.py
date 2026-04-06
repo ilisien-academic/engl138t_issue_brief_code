990579675751739348194432d4abbe9050897dcb
ilisien-academic <irl5102@psu.edu>
2026-04-06 16:10:50 +0000

    Apr 6, 2026, 12:10 PM

--- parse_ncf.py
+++ parse_ncf.py
@@ -6,7 +6,7 @@ import geopandas as gpd
 def parse_emission_ncf(in_path,voi):
     dataset = xarray.open_dataset(in_path)
 
-    only_voi = dataset[voi].isel(TSTEP=0,LAY=0)
+    only_voi = dataset[voi].isel(TSTEP=0,LAY=0)[::-1, :]
 
     # parameters specific to the CONUS IOAPI format (at least this NEMO dataset) obtained from julia NetCDF's "ncinfo"
     xorig = -2701000.25
