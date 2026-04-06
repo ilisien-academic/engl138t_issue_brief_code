a3179bbcd5af8e1fd640616fa037a660e60a56b0
ilisien-academic <irl5102@psu.edu>
2026-04-06 21:52:45 +0000

    Apr 6, 2026, 5:52 PM

--- nemo_data_viz.jl
+++ nemo_data_viz.jl
@@ -1,6 +1,7 @@
 using CairoMakie, NPZ, Rasters, Shapefile, GeoDataFrames
 
 pa_pm25_emis_nans = npzread("data/pa_pm25_emissions_data.npy")'
+
 pa_pm25_emis = ifelse.(isnan.(pa_pm25_emis_nans), missing, pa_pm25_emis_nans)
 
 fig = Figure()
