bebc0509c7f5971966c506c624b55a1e234ae812
ilisien-academic <irl5102@psu.edu>
2026-04-06 15:50:18 +0000

    Apr 6, 2026, 11:50 AM

--- parse_ncf.py
+++ parse_ncf.py
@@ -7,4 +7,16 @@ def parse_emission_ncf_to_tif(in_path,out_path,voi):
 
     only_voi = dataset["PM25ANN"].isel(TSTEP=0,LAY=0)
 
-    # parameters specific to the IOAPI format
+    # parameters specific to the CONUS IOAPI format (at least this NEMO dataset) obtained from julia NetCDF's "ncinfo"
+    xorig = -2701000.25
+    yorig = -1580581.38
+    xcell = 1000
+    ycell = 1000
+    ncols = 5397
+    nrows = 3177
+
+    transform = from_origin(xorig,yorig + ycell * nrows, xcell, ycell)
+
+    only_voi = only_voi.rio.write_crs("+proj=lcc +lat_1=38.5 +lat_2=38.5 +lat_0=38.5 +lon_0=-97.5 +datum=WGS84 +units=m")
+
+    only_voi.rio.write_transform(transform, inplace = Ture)
