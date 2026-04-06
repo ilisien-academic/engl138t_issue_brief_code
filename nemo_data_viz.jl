b2c10d8de1c7c1ed0dbdaf1acf73bb278269a704
ilisien-academic <irl5102@psu.edu>
2026-04-06 22:26:02 +0000

    Apr 6, 2026, 6:26 PM

--- nemo_data_viz.jl
+++ nemo_data_viz.jl
@@ -5,9 +5,14 @@ pa_pm25_emis = Raster("data/pa_pm25_emissions_data.tif")
 pa_census_tracts_up = GeoDataFrames.read("data/census_tracts/cb_2015_42_tract_500k.shp")
 pa_census_tracts = GeoDataFrames.reproject(pa_census_tracts_up, GeoFormatTypes.EPSG(4269), GeoFormatTypes.EPSG(2272))
 
-zonal(mean, pa_pm25_emis; geometrycolumn=:geometry, of=pa_census_tracts, boundary=:touches)
+function mean(something)
+    print(something)
+    return 2
+end
 
-print(zonal)
+mean_in_tracts = zonal(mean, pa_pm25_emis; geometrycolumn=:geometry, of=pa_census_tracts, boundary=:touches)
+
+print(mean_in_tracts)
 
 #=
 pa_pm25_emis_nans = npzread("data/pa_pm25_emissions_data.npy")'
