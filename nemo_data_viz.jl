385bb15e7732be562b9f8d339ef271c76028dd8d
ilisien-academic <irl5102@psu.edu>
2026-04-06 17:11:27 +0000

    Apr 6, 2026, 1:11 PM

--- nemo_data_viz.jl
+++ nemo_data_viz.jl
@@ -2,6 +2,7 @@ using CairoMakie
 using NPZ 
 
 pa_pm25_emis = npzread("data/pa_pm25_emissions_data.npy")'
+pa_pm25_emis .= ifelse.(isnan.(pa_pm25_emis), missing, pa_pm25_emis)
 
 fig = Figure()
 ax = Axis(fig[1,1],aspect=DataAspect())
