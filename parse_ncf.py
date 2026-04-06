695a4b6864f819565fd70e6889c6500f5d64d228
ilisien-academic <irl5102@psu.edu>
2026-04-06 02:08:50 +0000

    Apr 5, 2026, 10:08 PM

--- parse_ncf.py
+++ parse_ncf.py
@@ -1,4 +1,7 @@
 import netCDF4
 import numpy as np
 
-def parse_emissions(ncf_path,npy_path,)
+ALL_EMISSIONS_VARIABLES = ['VOCANN', 'NOXANN', 'COANN', 'SO2ANN', 'NH3ANN', 'PM25ANN', 'PECANN', 'POCANN', 'PMCANN']
+
+def parse_emissions(ncf_path,npy_path,emissions_variables=['PM25ANN']):
+    
