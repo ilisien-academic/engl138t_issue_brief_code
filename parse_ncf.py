afe16273af200a11910b6edd0f2d51efddb4600e
ilisien-academic <irl5102@psu.edu>
2026-04-06 15:46:12 +0000

    Apr 6, 2026, 11:46 AM

--- parse_ncf.py
+++ parse_ncf.py
@@ -2,4 +2,6 @@ import xarray
 import rioxarray
 from rasterio.transform import from_origin
 
-def parse_ncf_to_tif
+def parse_ncf_to_tif(in_path,out_path,variable_of):
+    dataset = xarray.open_dataset(in_path)
+
