0369b6e2719daf34db11e98faac8ad84cbdf5054
ilisien-academic <irl5102@psu.edu>
2026-04-06 16:42:29 +0000

    Apr 6, 2026, 12:42 PM

--- parse_ncf.py
+++ parse_ncf.py
@@ -43,7 +43,7 @@ def reproject_and_export(pa_voi,out_path):
 def raster_to_npy(in_path,out_path):
     pa_only_emissions_dataset = gdal.Open(in_path)
     np_array = np.array(pa_only_emissions_dataset.GetRasterBand(1).ReadAsArray())
-    numpy.save(out_path)
+    np.save(out_path,np_array)
 
 if __name__ == "__main__":
     ds = parse_emission_ncf("data/all_emissions_data.ncf","PM25ANN")
