1b3438d841a31778bfa18d8935e0c3cf172d87a7
ilisien-academic <irl5102@psu.edu>
2026-04-06 16:22:10 +0000

    Apr 6, 2026, 12:22 PM

--- parse_ncf.py
+++ parse_ncf.py
@@ -36,9 +36,9 @@ def clip_to_PA(voi_raw):
 
 def reproject_and_export(pa_voi,out_path):
     final_reprojected = pa_voi.rio.reproject("EPSG:2272")
-    final_reprojected.rio.to_raster(out_path)
+    final_reprojected.rio.to_numpy(out_path)
 
 if __name__ == "__main__":
     ds = parse_emission_ncf("data/all_emissions_data.ncf","PM25ANN")
     pa_ds = clip_to_PA(ds)
-    reproject_and_export(pa_ds,"data/pa_pm25_emissions_data.tif")
+    reproject_and_export(pa_ds,"data/pa_pm25_emissions_data.npy")
