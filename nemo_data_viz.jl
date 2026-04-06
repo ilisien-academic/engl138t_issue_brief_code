b92199133fa6238524f9e989187d15e185c136cd
ilisien-academic <irl5102@psu.edu>
2026-04-06 16:52:09 +0000

    Apr 6, 2026, 12:52 PM

--- nemo_data_viz.jl
+++ nemo_data_viz.jl
@@ -1,10 +1,10 @@
 using CairoMakie
 using NPZ 
 
-pa_pm25_emis = npzread("data/pa_pm25_emissions_data.npy")
+pa_pm25_emis = npzread("data/pa_pm25_emissions_data.npy")'
 
 fig = Figure()
-ax = Axis(fig[1,1],DataAspect())
+ax = Axis(fig[1,1],aspect=DataAspect())
 
 heatmap!(pa_pm25_emis)
 
