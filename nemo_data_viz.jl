762c3e1435dd2546ec7fd8293937a9c17a600ff7
ilisien-academic <irl5102@psu.edu>
2026-04-06 22:17:02 +0000

    Apr 6, 2026, 6:17 PM

--- nemo_data_viz.jl
+++ nemo_data_viz.jl
@@ -3,8 +3,8 @@ using CairoMakie, NPZ, Rasters, Shapefile, GeoDataFrames, GeoFormatTypes
 pa_pm25_emis = Raster("data/pa_pm25_emissions_data.tif")
 
 pa_census_tracts = GeoDataFrames.read("data/census_tracts/cb_2015_42_tract_500k.shp")
-
 pa_census_tracts = GeoFormatTypes.reproject(pa_census_tracts, GeoFormatTypes.EPSG(4269), GeoFormatTypes.EPSG(2272))
+
 
 
 pa_pm25_emis_nans = npzread("data/pa_pm25_emissions_data.npy")'
