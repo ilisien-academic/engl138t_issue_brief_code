ee39c102404d76e0832d4de98bff53abb041e694
ilisien-academic <irl5102@psu.edu>
2026-04-07 10:56:40 +0000

    Apr 7, 2026, 6:56 AM

--- nemo_data.jl
+++ nemo_data.jl
@@ -29,7 +29,7 @@ pa_census_tracts = GeoDataFrames.reproject(pa_census_tracts_up, GeoFormatTypes.E
 
 write("data/tmp_emissions.tif", pa_pm25_emis)
 tmp_tracts_string = GeoJSON.write(pa_census_tracts)
-PYgpd = 
+PYgpd = pyimport("geopandas")
 
 
 #geoms = pa_census_tracts.geometry
