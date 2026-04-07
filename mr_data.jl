e0953075393a47e3972fbf5a151f0c91a5da1b40
ilisien-academic <irl5102@psu.edu>
2026-04-07 22:55:52 +0000

    Apr 7, 2026, 6:55 PM

--- mr_data.jl
+++ mr_data.jl
@@ -72,8 +72,6 @@ pop_tall.age_group = map_pop_group.(pop_tall.age_group)
 pop_tall.population = [pop isa Float64 ? pop : missing for pop in pop_tall.population]
 deaths.rate = [death isa Float64 ? death : missing for death in deaths.rate]
 
-
-
 pop_grouped = DataFrames.combine(groupby(pop_tall,[:tract,:age_group]), :population => sum => :population)
 
 pop_and_deaths = innerjoin(deaths, pop_grouped, on=[:tract, :age_group])
