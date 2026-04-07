febb1ef6e93f0a11d396255fb9a9dbd09075d562
ilisien-academic <irl5102@psu.edu>
2026-04-07 07:55:25 +0000

    Apr 7, 2026, 3:55 AM

--- nemo_data.jl
+++ nemo_data.jl
@@ -24,14 +24,9 @@ pa_census_tracts = GeoDataFrames.reproject(pa_census_tracts_up, GeoFormatTypes.E
 pa_census_tracts[!,:mean_emis] .= 0.0
 geoms = pa_census_tracts.geometry
 
+
+
 @threads for i in eachindex(geoms)
-    geom = geoms[i]
-
-    if ismissing(geom)
-        pa_census_tracts[i, :mean_emis] = 0.0
-        continue
-    end
-
     pa_census_tracts[i, :mean_emis] = mean_raster_in_shape(pa_pm25_emis, geoms[i])
 end
 
