40ffc34fb6f230c655f823756b591c2ad4a188fd
ilisien-academic <irl5102@psu.edu>
2026-04-07 20:35:25 +0000

    Apr 7, 2026, 4:35 PM

--- mr_data.jl
+++ mr_data.jl
@@ -69,7 +69,7 @@ function map_pop_group(g)
 end
 
 pop_tall.age_group = map_pop_group.(pop_tall.age_group)
-pop_tall.population = [pop isa Float64 ? pop : tryparse(Float64,pop) for pop in pop_tall.population]
+pop_tall.population = [pop isa Float64 ? pop : missing for pop in pop_tall.population]
 deaths.rate = [death isa Float64 ? death : missing for death in deaths.rate]
 
 pop_grouped = DataFrames.combine(groupby(pop_tall,[:tract,:age_group]), :population => sum => :population)
