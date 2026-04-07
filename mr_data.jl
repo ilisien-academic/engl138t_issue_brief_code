5f0cf39baa1020ba506a68d88642310c10240fda
ilisien-academic <irl5102@psu.edu>
2026-04-07 16:43:36 +0000

    Apr 7, 2026, 12:43 PM

--- mr_data.jl
+++ mr_data.jl
@@ -1,7 +1,7 @@
 using CSV, DataFrames
 
 DEATHS_AGE_GROUPS= ["Under 1","1-4","5-14", "15-24", "25-34", "35-44", "45-54", "55-64", "65-74", "75-84", "85 and older"]
-POP_PREFIX = [""]
+POP_PREFIX = "Total!!Estimate!!AGE!!"
 
 pop = CSV.read("data/pop_and_death_data/pop.csv",DataFrame)
 deaths = CSV.read("data/pop_and_death_data/death.csv",DataFrame)
