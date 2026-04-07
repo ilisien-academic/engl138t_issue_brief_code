9bb2fb8f88021e95ee8bc698c068cd7e17565939
ilisien-academic <irl5102@psu.edu>
2026-04-07 16:46:21 +0000

    Apr 7, 2026, 12:46 PM

--- mr_data.jl
+++ mr_data.jl
@@ -1,6 +1,18 @@
 using CSV, DataFrames
 
-DEATHS_AGE_GROUPS = ["Under 1","1-4","5-14", "15-24", "25-34", "35-44", "45-54", "55-64", "65-74", "75-84", "85 and older"]
+DEATHS_AGE_GROUPS = ["Under 1",
+                     "1-4",
+                     "5-14",
+                     "15-24",
+                     "25-34",
+                     "35-44",
+                     "45-54",
+                     "55-64",
+                     "65-74",
+                     "75-84",
+                     "85 and older"
+]
+
 POP_PREFIX = "Total!!Estimate!!AGE!!"
 POP_AGE_GROUPS = POP_PREFIX .* [
     "Under 5 years",
@@ -22,6 +34,7 @@ POP_AGE_GROUPS = POP_PREFIX .* [
     "80 to 84 years",
     "85 years and over"
 ]
+
 pop = CSV.read("data/pop_and_death_data/pop.csv",DataFrame)
 deaths = CSV.read("data/pop_and_death_data/death.csv",DataFrame)
 
