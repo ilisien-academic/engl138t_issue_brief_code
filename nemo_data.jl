8739c0d8d9d97d00ab49dad859d01bd28b7f5d8e
ilisien-academic <irl5102@psu.edu>
2026-04-07 00:43:33 +0000

    Apr 6, 2026, 8:43 PM

--- nemo_data.jl
+++ nemo_data.jl
@@ -8,7 +8,7 @@ end
 
 pa_pm25_emis_tif = Raster("data/pa_pm25_emissions_data.tif")
 npy_data = npzread("data/pa_pm25_emissions_data.npy")
-pa_pm25_emis = Raster(npy_data, dims(pa_pm25_emis_tif))
+pa_pm25_emis = Raster(npy_data', dims(pa_pm25_emis_tif))
 
 pa_census_tracts_up = GeoDataFrames.read("data/census_tracts/cb_2015_42_tract_500k.shp")
 pa_census_tracts = GeoDataFrames.reproject(pa_census_tracts_up, GeoFormatTypes.EPSG(4269), GeoFormatTypes.EPSG(2272))
