0ed8a8b17ad6ff2f52812724e9328967f75dd39b
ilisien-academic <irl5102@psu.edu>
2026-04-07 16:57:43 +0000

    Apr 7, 2026, 12:57 PM

--- mr_data.jl
+++ mr_data.jl
@@ -42,5 +42,32 @@ pop.Geography = [i[2] for i in split.(pop.Geography,"S")]
 
 pop_tall = DataFrames.stack(pop, POP_AGE_GROUPS; variable_name = :pop_age_group, value_name = :population)
 
+rename!(pop_tall, :Geography => :tract)
 
+function map_pop_group(g)
+    if occursin("Under 5", g)
+        return "Under 5"
+    elseif occursin("5 to 9", g) || occursin("10 to 14", g)
+        return "5-14"
+    elseif occursin("15 to 19", g) || occursin("20 to 24", g)
+        return "15-24"
+    elseif occursin("25 to 29", g) || occursin("30 to 34", g)
+        return "25-34"
+    elseif occursin("35 to 39", g) || occursin("40 to 44", g)
+        return "35-44"
+    elseif occursin("45 to 49", g) || occursin("50 to 54", g)
+        return "45-54"
+    elseif occursin("55 to 59", g) || occursin("60 to 64", g)
+        return "55-64"
+    elseif occursin("65 to 69", g) || occursin("70 to 74", g)
+        return "65-74"
+    elseif occursin("75 to 79", g) || occursin("80 to 84", g)
+        return "75-84"
+    else
+        return "85 and older"
+    end
+end
 
+pop_tall.pop_age_group = map_pop_group.(pop_tall.pop_age_group)
+
+pop_grouped = combine(groupby(pop_tall,[:tract,:age]))
