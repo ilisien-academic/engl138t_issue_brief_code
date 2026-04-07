d68abc8e34ebf00d2e7c757e6434ee8f7428064f
ilisien-academic <irl5102@psu.edu>
2026-04-07 16:52:47 +0000

    Apr 7, 2026, 12:52 PM

--- mr_data.jl
+++ mr_data.jl
@@ -39,3 +39,5 @@ pop = CSV.read("data/pop_and_death_data/pop.csv",DataFrame)
 deaths = CSV.read("data/pop_and_death_data/death.csv",DataFrame)
 
 pop.Geography = [i[2] for i in split.(pop.Geography,"S")]
+
+pop_long 
