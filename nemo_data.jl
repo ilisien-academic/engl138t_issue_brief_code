cddad9b63a400b1742912c28027e35598ae51980
ilisien-academic <irl5102@psu.edu>
2026-04-07 00:10:20 +0000

    Apr 6, 2026, 8:10 PM

--- nemo_data.jl
+++ nemo_data.jl
@@ -1,7 +1,7 @@
 using CairoMakie, NPZ, Rasters, Shapefile, GeoDataFrames, GeoFormatTypes, Statistics, LinearAlgebra
 
-function mean_raster_in_shape(raster_wo_missings,shape)
-    cvg = Rasters.coverage(shape;to=raster,threaded,scale=25)
+function mean_raster_in_shape(cvg,raster_wo_missings,shape)
+    Rasters.coverage!(shape;to=cvg,threaded,scale=25)
     return cdot(cvg,raster_wo_missings)/sum(cvg)
 
 pa_pm25_emis = Raster("data/pa_pm25_emissions_data.tif")
