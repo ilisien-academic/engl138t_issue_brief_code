381675228903b246adc643a90a04e39c1574c026
ilisien-academic <irl5102@psu.edu>
2026-04-07 07:57:54 +0000

    Apr 7, 2026, 3:57 AM

--- nemo_data.jl
+++ nemo_data.jl
@@ -5,7 +5,8 @@ function mean_raster_in_shape(raster, shape)
     cropped = crop(raster; to=shape, pad=true)
     cvg = Rasters.coverage(shape; to=cropped, scale=10)
     masked_raster = mask(cropped; with=shape)
-    return sum(cvg .* masked_raster) / sum(cvg)
+    total_cvg = sum(cvg)
+    return total_cvg == 0 ? 0.0 : sum(cvg .* masked_raster) / total_cvg
 end
 
 pa_pm25_emis_tif = Raster("data/pa_pm25_emissions_data.tif")
