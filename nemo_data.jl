893d29233e30c989a21c66d9e023b6ad77de857f
ilisien-academic <irl5102@psu.edu>
2026-04-07 06:43:21 +0000

    Apr 7, 2026, 2:43 AM

--- nemo_data.jl
+++ nemo_data.jl
@@ -6,7 +6,7 @@ ax = Axis(fig[1,1],aspect=DataAspect())
 
 function mean_raster_in_shape(cvg,raster_wo_missings,shape)
     print(shape)
-    Rasters.coverage!(cvg,shape;scale=10)
+    Rasters.coverage!(cvg,shape;scale=50)
     return sum(cvg) #dot(cvg,raster_wo_missings)/sum(cvg)
 end
 
