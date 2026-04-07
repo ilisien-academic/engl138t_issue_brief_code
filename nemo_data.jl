f4a2807ac6fff7c6e66f5c54fec20860bac1928c
ilisien-academic <irl5102@psu.edu>
2026-04-07 07:18:22 +0000

    Apr 7, 2026, 3:18 AM

--- nemo_data.jl
+++ nemo_data.jl
@@ -41,7 +41,6 @@ poly!(ax,
       pa_census_tracts.geometry;
       color = pa_census_tracts.mean_emis,
       colormap = :plasma,
-      colorrange = clims,
       strokecolor = :white,
       strokewidth = 0.3
 )
