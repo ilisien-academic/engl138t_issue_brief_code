bf4c97f1d1a8072fad95c06a56eefe6d19e89e18
ilisien-academic <irl5102@psu.edu>
2026-04-07 16:55:35 +0000

    Apr 7, 2026, 12:55 PM

--- mr_data.jl
+++ mr_data.jl
@@ -40,5 +40,7 @@ deaths = CSV.read("data/pop_and_death_data/death.csv",DataFrame)
 
 pop.Geography = [i[2] for i in split.(pop.Geography,"S")]
 
-pop_tall = stack(pop, POP_AGE_GROUPS; variable_name = :pop_age_group, value_name = :population)
+pop_tall = DataFrames.stack(pop, POP_AGE_GROUPS; variable_name = :pop_age_group, value_name = :population)
+
+
 
