85ca1615027c56dc8a4ba7a922356857d4f357e7
ilisien-academic <irl5102@psu.edu>
2026-04-07 15:50:36 +0000

    Apr 7, 2026, 11:50 AM

--- nemo_data.jl
+++ nemo_data.jl
@@ -16,6 +16,7 @@ coalesce.(pa_pm25_emis, 0.0)
 pa_census_tracts_up = GeoDataFrames.read("data/census_tracts/cb_2015_42_tract_500k.shp")
 pa_census_tracts = GeoDataFrames.reproject(pa_census_tracts_up, GeoFormatTypes.EPSG(4269), GeoFormatTypes.EPSG(2272))
 
+# horrendous workaround but it works!
 Rasters.write("data/tmp_emissions.tiff", pa_pm25_emis, force=true)
 tmp_tracts_string = GeoJSON.write(pa_census_tracts)
 PYgpd = pyimport("geopandas")
