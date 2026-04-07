16f8d908003302f11fd58eaba27f60ef15cfdd27
ilisien-academic <irl5102@psu.edu>
2026-04-07 00:14:03 +0000

    Apr 6, 2026, 8:14 PM

--- nemo_data.jl
+++ nemo_data.jl
@@ -11,8 +11,8 @@ pa_pm25_emis = Raster("data/pa_pm25_emissions_data.tif")
 pa_census_tracts_up = GeoDataFrames.read("data/census_tracts/cb_2015_42_tract_500k.shp")
 pa_census_tracts = GeoDataFrames.reproject(pa_census_tracts_up, GeoFormatTypes.EPSG(4269), GeoFormatTypes.EPSG(2272))
 
-Threads.@threads for tract in eachindex(pa_census_tracts[!,:geometry]
-    
+Threads.@threads for i in eachindex(pa_census_tracts[!,:geometry])
+    pa_census_tracts
 end
 
 
