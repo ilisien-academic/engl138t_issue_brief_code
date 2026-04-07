d88c87a282c2f851d358710a6a92037a82db9943
ilisien-academic <irl5102@psu.edu>
2026-04-07 16:37:19 +0000

    Apr 7, 2026, 12:37 PM

--- mr_data.jl
+++ mr_data.jl
@@ -3,4 +3,4 @@ using CSV, DataFrames
 pop = CSV.read("data/pop_and_death_data/pop.csv",DataFrame)
 deaths = CSV.read("data/pop_and_death_data/death.csv",DataFrame)
 
-pop.GEO_ID = 
+pop.Geography = [i[2] for i in split.(pop.Geography,"S")]
