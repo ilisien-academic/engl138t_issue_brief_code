b2383b35d3a0087000cd3e5ebe3ddf354e321f71
ilisien-academic <irl5102@psu.edu>
2026-04-10 05:14:24 +0000

    Apr 10, 2026, 1:14 AM

--- mr_nemo_combined.jl
+++ mr_nemo_combined.jl
@@ -16,9 +16,6 @@ function annual_ap_deaths_per_x_people(pm, mr)
 end
 
 transform!(mrpm, [:mean_emis,:mortality_rate] => annual_ap_deaths_per_x_people => :expected_ann_deaths_per_1000)
-dropmissing!(mrpm)
-
-CSV.write("data/why.csv",mrpm)
 
 fig = Figure()
 ax = Axis(fig[1,1],aspect=DataAspect())
