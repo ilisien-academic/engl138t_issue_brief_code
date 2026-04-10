592794949158a0e40ec494ef57c82c13fa0bedad
ilisien-academic <irl5102@psu.edu>
2026-04-10 03:07:46 +0000

    Apr 9, 2026, 11:07 PM

--- mr_data.jl
+++ mr_data.jl
@@ -92,6 +92,7 @@ pop_and_deaths = DataFrames.combine(
 
 # checking the totals of populations (all around 100, could possibly normalize) println(DataFrames.combine(groupby(pop_tall, :tract), :population => sum => :total))
 
+DataFrames.combine(groupby(pop_tall, :tract), :population => sum => :total)
 
 
 CSV.write("data/pop_and_death_data/pop_and_deaths.csv", pop_and_deaths)
