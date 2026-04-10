45041915d8c799f5419fe20e410efaac43988ca2
ilisien-academic <irl5102@psu.edu>
2026-04-10 03:12:48 +0000

    Apr 9, 2026, 11:12 PM

--- mr_data.jl
+++ mr_data.jl
@@ -94,6 +94,10 @@ pop_and_deaths = DataFrames.combine(
 
 CSV.write("data/pop_and_death_data/pop_and_deaths.csv", pop_and_deaths)
 
+function norm_to_100(unnormed)
+    return unnormed / sum(unnormed)
+end
+
 function get_mr(deaths, population)
     
 end
