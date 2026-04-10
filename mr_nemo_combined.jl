438c67e1980b9cec564931b6d9c33b2d8b0c6008
ilisien-academic <irl5102@psu.edu>
2026-04-10 05:10:28 +0000

    Apr 10, 2026, 1:10 AM

--- mr_nemo_combined.jl
+++ mr_nemo_combined.jl
@@ -16,12 +16,13 @@ function annual_ap_deaths_per_x_people(pm, mr)
 end
 
 transform!(mrpm, [:mean_emis,:mortality_rate] => annual_ap_deaths_per_x_people => :expected_ann_deaths_per_1000)
+dropmissing!(mrpm)
 
 fig = Figure()
 ax = Axis(fig[1,1],aspect=DataAspect())
 
 poly!(ax,
-      skipmissing(mrpm.geometry);
+      mrpm.geometry;
       color = mrpm.expected_ann_deaths_per_1000,
       colormap = :plasma,
 )
