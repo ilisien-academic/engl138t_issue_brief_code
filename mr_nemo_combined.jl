5c71c31d336a1b38a7ae528e369063ebe0aa8ee1
ilisien-academic <irl5102@psu.edu>
2026-04-10 07:56:32 +0000

    Apr 10, 2026, 3:56 AM

--- mr_nemo_combined.jl
+++ mr_nemo_combined.jl
@@ -38,4 +38,4 @@ poly!(ax,
 
 save("figs/fig1.svg",fig)
 
-transform!(mrpm, [:expected_ann_deaths_per_1000,:population] => ((x,y) -> x .* y) => :expected_ann_deaths)
+transform!(mrpm, [:expected_ann_deaths_per_1000,:population] => ((x,y) -> x .* y ./ 1000) => :expected_ann_deaths)
