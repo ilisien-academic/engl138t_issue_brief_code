4021e18250309c9e90e1f0d87403cd83be7a1f45
ilisien-academic <irl5102@psu.edu>
2026-04-10 03:31:37 +0000

    Apr 9, 2026, 11:31 PM

--- mr_nemo_combined.jl
+++ mr_nemo_combined.jl
@@ -1,5 +1,7 @@
 using CSV, DataFrames
 
+RR_AP # from Lepeule
+
 mr = CSV.read("data/mortality_rates.csv",DataFrame)
 pm = CSV.read("data/pm25s.csv",DataFrame)
 
@@ -7,4 +9,3 @@ rename!(pm, :GEOID => :tract)
 
 mrpm = rightjoin(pm, mr, on = :tract)
 
-mrpm
