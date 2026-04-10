b067aa0c534dcbed4f2a079c12e63bef22bab772
ilisien-academic <irl5102@psu.edu>
2026-04-10 05:03:44 +0000

    Apr 10, 2026, 1:03 AM

--- mr_nemo_combined.jl
+++ mr_nemo_combined.jl
@@ -11,6 +11,7 @@ rename!(pm, :GEOID => :tract)
 mrpm = rightjoin(pm, mr; on = :tract)
 
 function annual_ap_deaths_per_x_people(pm, mr)
+    print(PER_HOW_MANY_PEOPLE / (1 .- (RR_AP * (mr .* pm))))
     return PER_HOW_MANY_PEOPLE / (1 .- (RR_AP * (mr .* pm)))
 end
 
