1937e6f88cff731df5591500c339154941ecdc18
ilisien-academic <irl5102@psu.edu>
2026-04-16 00:20:27 +0000

    Apr 15, 2026, 8:20 PM

--- improved_sankey.jl
+++ improved_sankey.jl
@@ -4,4 +4,7 @@ sources = CSV.read("data/by_type.csv",DataFrame)
 
 dropmissing!(sources, :pm25)
 
-sources.source_label = [x == 1 ? "Commercial": "Non-Commercial" for x in sources.]
+sources.source_label = [x == 1 ? "Commercial" : "Non-Commercial" for x in sources.commercial_source]
+
+summary = combine(groupby(sources, :source_label), :pm25 => sum => :total_emissions)
+println("pollution totals:")
