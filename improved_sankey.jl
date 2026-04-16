537660f62e196de750656c4a624e339510a78c44
ilisien-academic <irl5102@psu.edu>
2026-04-16 00:18:24 +0000

    Apr 15, 2026, 8:18 PM

--- improved_sankey.jl
+++ improved_sankey.jl
@@ -2,4 +2,6 @@ using SankeyMakie, CSV, CairoMakie, DataFrames
 
 sources = CSV.read("data/by_type.csv",DataFrame)
 
-dropmissing!(sources, :"")
+dropmissing!(sources, :pm25)
+
+sources.source_label = [x == 1 ? "Commercial": "Non-Commercial" for x in sources.]
