8425ce3ee79288171b08d8b32b97ec6348f6bfae
ilisien-academic <irl5102@psu.edu>
2026-04-16 01:05:10 +0000

    Apr 15, 2026, 9:05 PM

--- nemo_data.jl
+++ nemo_data.jl
@@ -3,7 +3,7 @@ ENV["JULIA_PYTHONCALL_EXE"] = "env/scripts/python.exe"
 using CairoMakie, NPZ, Rasters, Shapefile, GeoDataFrames, GeoFormatTypes, Statistics, LinearAlgebra, PythonCall, GeoJSON, DataFrames, CSV
 using Base.Threads
 
-pa_pm25_emis = Raster("data/pa_pm25_emissions_data_v2.tif")
+pa_pm25_emis = Raster("data/pa_pm25_emissions_data.tif")
 #npy_data = npzread("data/pa_pm25_emissions_data.npy")
 #x_range = range(1.0196644610284471e6, 2.9405851371205086e6, size(npy_data, 2))
 #y_range = range(1.272233114741428e6, -176652.4464065095, size(npy_data, 1))
