2c516b9c43a25f928bf16d00a829e9aafdbdb02a
ilisien-academic <irl5102@psu.edu>
2026-04-16 00:21:52 +0000

    Apr 15, 2026, 8:21 PM

--- improved_sankey.jl
+++ improved_sankey.jl
@@ -9,3 +9,5 @@ sources.source_label = [x == 1 ? "Commercial" : "Non-Commercial" for x in source
 summary = combine(groupby(sources, :source_label), :pm25 => sum => :total_emissions)
 println("pollution totals: $summary")
 
+sankey_collate = combine(groupby(sources, [:desc, :source_label]), :pm25 => sum => :value)
+
